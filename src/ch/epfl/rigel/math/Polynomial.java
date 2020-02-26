200ec65ef089bc56ebfee9c1a5c8ac798ca90947
Ciacco99 <jacopo.ferro@epfl.ch>
2020-02-26 21:18:14 +0100

    Polynomial fixed
    Signed-off-by: Ciacco99 <jacopo.ferro@epfl.ch>

--- src/ch/epfl/rigel/math/Polynomial.java
+++ src/ch/epfl/rigel/math/Polynomial.java
@@ -4,14 +4,12 @@ public final class Polynomial {
 
 // polynomial is represented with exponents in decreasing order
     private final double[] polynomials;
-    private Polynomial(double coefficientN, double... coefficients){
+    private Polynomial(double coefficientN, double[] coefficients){
         this.polynomials = new double[coefficients.length + 1];
-        this.polynomials[polynomials.length-1] = coefficientN;
-        for(double coefficient: coefficients){
-            for(int i = 0; i<=polynomials.length - 2; i++){
-                this.polynomials[i] = coefficient;
+        this.polynomials[0] = coefficientN;
+            for(int i = 1; i<=coefficients.length; i++){
+                this.polynomials[i] = coefficients[i-1];
             }
-        }
 
     }
 
@@ -26,7 +24,12 @@ public final class Polynomial {
             throw new IllegalArgumentException();
         }
         else {
-            return new Polynomial(coefficientN, coefficients);
+           //copy the values of the ellipse in  proper array
+            double[] coeff = new double[coefficients.length];
+                for(int i = 0; i<coefficients.length; i++){
+                    coeff[i] = coefficients[i];
+                }
+            return new Polynomial(coefficientN, coeff);
         }
     }
 
@@ -35,7 +38,7 @@ public final class Polynomial {
      * @param x
      * @return evaluate polynomial for given value
      */
-    public double at(double x){
+ /**   public double at(double x){
         int n = polynomials.length - 1;
         double y = polynomials[n];
         for (int i = n - 1; i >= 0; i--) {
@@ -43,18 +46,33 @@ public final class Polynomial {
         }
         return y;
     }
+**/
+    /**
+     *
+     * @param x
+     * @return evaluate polynomial for given value
+     */
+    public double at(double x){
+     int n = polynomials.length ;
+     //initialize result: we are sure that there is at least one element in polynomials so n=0 as a minimum
+     double result = polynomials[0];
 
-    public String toString(){
+     for (int i = 1; i<n; i++) {
+             result = polynomials[i] + x*result;
+         }
+         return result;
+     }
+  /**  public String toString(){
 
     }
-
+**/
     /**
      *
      * @return string representation of polynomial ignoring any 0 coefficients
      */
 
-/**
-    public String toString(){
+
+/*    public String toString(){
         if(polynomials.length > 1){
             String out = polynomials[polynomials.length - 1] + "x^" + (polynomials.length - 1);
             for(int i = polynomials.length - 2; i>=2; i--){
@@ -88,7 +106,7 @@ public final class Polynomial {
         }
 
     }
-**/
+*/
     /**
     public String toString(){
         // returns String rep ignoring any 0 coefficients
@@ -108,6 +126,121 @@ public final class Polynomial {
 
     }
      **/
+
+/*
+    public String toString(){
+        String out = new String() ;
+        StringBuilder builder = new StringBuilder( out);
+        int exp = polynomials.length-1;
+        for(double coef : polynomials ){
+            if(coef==polynomials[0] && exp>1) {
+                if( coef ==1)  builder.append( "x^"+ exp);
+                else if (coef == -1) builder.append( "-x^"+ exp);
+                else builder.append(coef +"x^"+ exp);
+            }
+            else if( coef > 0 && coef !=1 &&  coef!=polynomials[0]){
+                if(exp!= 1 && exp!=0 && coef!=1 && coef!=-1) builder.append("+" +coef +"x^"+ exp);
+
+                else if( exp == 1 && coef!=1 && coef!=-1)builder.append("+"+coef +"x");
+
+                else if(exp == 0 ) builder.append("+"+coef);
+                else if ( coef ==1)  builder.append( "x^"+ exp);
+                else if (coef == -1) builder.append( "-x^"+ exp);
+            }
+            else if( coef < 0 && coef != 1 && coef!=polynomials[0]){
+                if(exp!= 1 && exp!=0  ) builder.append(coef +"x^"+ exp);
+
+                else if( exp == 1 && coef!=1 && coef!=-1) builder.append(coef +"x");
+
+                else if(exp == 0 ) builder.append(coef);
+                else if ( coef ==1)  builder.append( "x^"+ exp);
+                else if (coef == -1) builder.append( "-x^"+ exp);
+            }
+            else if(exp ==1) builder.append(coef + "x");
+            else builder.append(coef);
+             exp -= 1;
+        }
+
+
+
+        out = builder.toString();
+        return out;
+
+    }
+*/
+    public String toString(){
+        String out = new String() ;
+        StringBuilder builder = new StringBuilder( out);
+        int exp = polynomials.length-1;
+        for(double coef : polynomials ){
+            if(polynomials.length ==1 )  builder.append(coef);
+            else if (polynomials.length ==2){
+                if(exp==1) builder.append(coef + "x");
+                else if(coef >0) builder.append("+"+ coef);
+                else if( coef<0) builder.append(coef);
+
+            }
+
+            else {
+                if(coef>0){
+                    if (coef!=1){
+                        if (exp == polynomials.length - 1) builder.append(coef + "x^" + exp);
+
+                        else if (exp == 1) builder.append("+" + coef + "x");
+
+                        else if (exp == 0) builder.append("+"+coef);
+
+                        else builder.append("+" + coef + "x^" + exp);
+                    }
+                    else{
+                        if (exp == polynomials.length - 1) builder.append("x^" + exp);
+
+                        else if (exp == 1) builder.append("+x");
+
+                        else if (exp == 0) builder.append(coef);
+
+                        else builder.append("+x^" + exp);
+                    }
+                }
+                else if(coef<0){
+                    if(coef!=-1){
+                        if(exp==polynomials.length-1 && exp!= 1) builder.append(coef + "x^"+exp);
+
+                        else if (exp==1) builder.append(coef + "x");
+
+                        else if(exp==0) builder.append(coef);
+
+                        else builder.append( coef + "x^"+ exp);
+
+                    }
+                    else{
+                        if(exp==polynomials.length-1 && exp!= 1) builder.append("-x^"+exp);
+
+                        else if (exp==1) builder.append("-x");
+
+                        else if(exp==0) builder.append(coef);
+
+                        else builder.append("-x^"+ exp);
+
+                    }
+
+                }
+                else if(coef==0){
+                    //niente
+                }
+            }
+
+
+            exp -= 1;
+        }
+
+
+
+        out = builder.toString();
+        return out;
+
+    }
+
 
     @Override
     public int hashCode(){
