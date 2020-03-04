9d79baee514fe99a85265c49700b7082815cfd9f
alptheexplorer <alpozen@gmail.com>
2020-03-04 14:02:41 +0100

    checking out

--- src/ch/epfl/rigel/astronomy/SiderealTime.java
+++ src/ch/epfl/rigel/astronomy/SiderealTime.java
@@ -31,6 +31,8 @@ public final class SiderealTime {
         ZonedDateTime convertedToGW = when.withZoneSameInstant(ZoneId.of("Europe/Greenwich"));
         bigT = J2000.julianCenturiesUntil(convertedToGW.truncatedTo(ChronoUnit.DAYS));
         smallT = convertedToGW.getHour();
+
+
         S_0 = 0.000025862*bigT*bigT + 2400.051336*bigT + 6.697374558;
         S_1 = 1.002737909*smallT;
         result = S_0 + S_1;
